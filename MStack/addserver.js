5ce31777ab5c9d944d1179458bf6c02a8af9d28c
aishas97 <43925536+aishas97@users.noreply.github.com>
2019-12-27 16:08:50 +0530

    Add files via upload

---
+++ MStack/addserver.js
@@ -0,0 +1,42 @@
+fs=require('fs');
+http=require("http");
+url=require("url");
+query=require("querystring");
+module=require("./addmodule")
+processdata=function(req,resp){
+d=url.parse(req.url);
+console.log(d);
+switch(d.pathname)
+{
+	case "/":
+	resp.writeHead(200,{'Content-Type':'text/html'})
+	fs.readFile("addhtml.html",function(error,data)
+	
+	{
+	if(error)
+	{
+		console.log("error occured");
+	}
+	else{
+		resp.end(data);
+	}
+	});
+	break;
+	case "/calculate":
+   resp.writeHead(200,{'Content-Type':'text/html'})
+   data=query.parse(d.query);
+   resp.write("data is :"+data.num1+"--->"+data.num2);
+   resp.end("Addition :"+addmodule.addition(data.num1,data.num2));
+
+   
+   break;
+default:
+   resp.writeHead(200,{'Content-Type':'text/html'})
+   resp.end("<h1>page not found</h1>");
+   break;
+
+
+}
+}
+http.createServer(processdata).listen(3000);
+console.log("server is running at 3000");
